ff6b0dd34f38e85a518b9f8c92b17628b84b9f1f
GunnerSaurusBJJ <ddaa.99ars@yandex.ru>
2020-05-03 13:50:53 +0300

    add count

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <h1>Todo app</h1>
+    <div class="count">{{allTodos.length}}</div>
     <addTodo />
     <listTodo />
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import addTodo from "@/components/addTodo";
 import listTodo from "@/components/listTodo";
 export default {
   components: { addTodo, listTodo },
-  name: "App"
+  name: "App",
+  computed: {
+    ...mapGetters(["allTodos"])
+  }
 };
 </script>
 
@@ -36,9 +41,18 @@ html {
   border: 2px solid;
   border-radius: 10px;
   background: rgba(255, 255, 255, 0.2);
+  position: relative;
 }
 h1 {
   text-transform: uppercase;
   letter-spacing: 2px;
+}
+.count {
+  position: absolute;
+  top: 17px;
+  right: 20px;
+  text-transform: uppercase;
+  font-weight: 900;
+  font-size: 30px;
 }
 </style>
